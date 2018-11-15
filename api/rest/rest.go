a744b8bb5f2130c0d77cc2091551a126ba86b67d
Addminus <adrianweniger@gmail.com>
2018-11-15 11:49:20 +0100

    ist alivegit statusgit status

--- api/rest/rest.go
+++ api/rest/rest.go
@@ -2,67 +2,107 @@ package main
 
 import (
 	"log"
-
+	// "context"
+	"encoding/json"
+	"fmt"
 	"github.com/emicklei/go-restful"
-
-	hello "github.com/micro/examples/greeter/srv/proto/hello"
-	"github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/broker"
+	"github.com/micro/go-plugins/broker/nats"
 	"github.com/micro/go-web"
-
-	"context"
+	// "github.com/nats-io/go-nats"
 )
-
-type Say struct{}
 
 var (
-	cl hello.SayService
+	// natsConn            *nats.Conn
+	natsServerAddress   string = "nats://nats:IoslProject2018@iosl2018hxqma76gup7si-vm0.westeurope.cloudapp.azure.com:4222"
+	natsRawSimDataQueue string = "GoMicro_SimulatorData"
 )
 
-// thingies are is processed locally
-func (s *Say) Anything(req *restful.Request, rsp *restful.Response) {
-	log.Print("Received Say.Anything API request")
-	rsp.WriteEntity(map[string]string{
-		"message": "Hi, this is the Greeter API",
-	})
+type SimulatorApi struct{}
+type MockupData struct {
+	MessageId int
+	CarId     int
+	Timestamp string
+	Accuracy  int
+	Lat       float32
+	Lon       float32
 }
 
-// thingies are processed in /svr
-func (s *Say) Hello(req *restful.Request, rsp *restful.Response) {
-	log.Print("Received Say.Hello API request")
+//post mockup simulation data via rest API
+func (s *SimulatorApi) PushData(req *restful.Request, rsp *restful.Response) {
+	log.Print("Received SimulatorApi.PushData API request")
 
-	name := req.PathParameter("name")
+	// natsConn.Subscribe(natsRawSimDataQueue, func(m *nats.Msg) {
+	// 	fmt.Printf("Received a message: %s\n", string(m.Data))
+	// })
 
-	response, err := cl.Hello(context.TODO(), &hello.Request{
-		Name: name,
+	// mockUp := MockupData{
+	// 	MessageId: 1,
+	// 	CarId:     1,
+	// 	Timestamp: "xx-xx-xxx",
+	// 	Accuracy:  1,
+	// 	Lat:       1.23,
+	// 	Lon:       2.34,
+	// }
+	// mockUpJson, err := json.Marshal(mockUp)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// push data to NATS Queue
+	// natsConn.Publish(natsRawSimDataQueue, mockUpJson)
+	// brokershared.Publish(natsRawSimDataQueue, mockUpJson)
+
+	rsp.WriteEntity(map[string]string{
+		"message": "pushed mockup dimulation data to NATS queue",
 	})
-
-	if err != nil {
-		rsp.WriteError(500, err)
-	}
-
-	rsp.WriteEntity(response)
 }
 
 func main() {
+	natsBroker := nats.NewBroker(broker.Addrs(natsServerAddress))
+
 	// Create service
 	service := web.NewService(
-		web.Name("go.micro.api.greeter"),
+		web.Name("go.micro.api.mockup"),
 	)
 
 	service.Init()
+	natsBroker.Connect()
+	mockUp := MockupData{
+		MessageId: 1,
+		CarId:     1,
+		Timestamp: "xx-xx-xxx",
+		Accuracy:  1,
+		Lat:       1.23,
+		Lon:       2.34,
+	}
+	mockUpJson, err := json.Marshal(mockUp)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// setup Greeter Server Client
-	cl = hello.NewSayService("go.micro.srv.greeter", client.DefaultClient)
+	msg := broker.Message{
+		Header: map[string]string{},
+		Body:   mockUpJson,
+	}
+
+	natsBroker.Publish(natsRawSimDataQueue, &msg)
+	fmt.Printf("published")
+	// nc, err := nats.Connect(natsServerAddress)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	natsConn = nc
+	// }
 
 	// Create RESTful handler
-	say := new(Say)
+	simAPI := new(SimulatorApi)
 	ws := new(restful.WebService)
 	wc := restful.NewContainer()
 	ws.Consumes(restful.MIME_XML, restful.MIME_JSON)
 	ws.Produces(restful.MIME_JSON, restful.MIME_XML)
-	ws.Path("/greeter")
-	ws.Route(ws.GET("/").To(say.Anything))
-	ws.Route(ws.GET("/{name}").To(say.Hello))
+	ws.Path("/simulator")
+	ws.Route(ws.POST("/").To(simAPI.PushData))
 	wc.Add(ws)
 
 	// Register Handler
--- map-matcher/main.go
+++ map-matcher/main.go
@@ -9,12 +9,14 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/broker"
 	"github.com/micro/go-plugins/broker/nats"
+	// "github.com/nats-io/go-nats"
 )
 
 var (
 	natsURI            = "nats://nats:IoslProject2018@iosl2018hxqma76gup7si-vm0.westeurope.cloudapp.azure.com:4222"
 	subscribeQueueName = "GoMicro_SimulatorData"
 	publishQueueName   = "GoMicro_MapMatcher"
+	// natsConn           *nats.Conn
 )
 
 // MockedReceivingMessage comment
@@ -34,7 +36,7 @@ func main() {
 		micro.Name("go.micro.mapmatcher"),
 		micro.RegisterTTL(time.Second*30),
 		micro.RegisterInterval(time.Second*10),
-		micro.Broker(natsBroker),
+		// micro.Broker(natsBroker),
 	)
 
 	// optionally setup command line usage
@@ -46,32 +48,48 @@ func main() {
 	natsBroker.Subscribe(
 		subscribeQueueName,
 		broker.Handler(func(p broker.Publication) error {
-			fmt.Printf("HELLOOO!!!")
 			var msgBody = p.Message().Body
-			fmt.Printf("HELLO!")
-			fmt.Printf(string(msgBody))
+			// fmt.Printf(msgBody)
+			// rawIn := json.RawMessage(msgBody)
 			var msg MockedReceivingMessage
-			err := json.Unmarshal(msgBody, &msg)
+			rawIn := json.RawMessage(msgBody)
+			bytes, err := rawIn.MarshalJSON()
 			if err != nil {
+				panic(err)
+			}
+			err2 := json.Unmarshal(bytes, &msg)
+			if err2 != nil {
 				fmt.Println("error:", err)
 			}
+			fmt.Printf("%+v\n", msg)
 			processMessage(MockedReceivingMessage(msg))
 			return nil
 		}),
 	)
 
-	// var msg = broker.Message{
-	// 	map[string]string{},
-	// 	[]byte("Hello NATS!"),
+	// nc, err := nats.Connect(natsURI)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	natsConn = nc
 	// }
 
-	// natsBroker.Publish(
-	// 	publishQueueName,
-	// 	&msg,
-	// )
+	// natsConn.Subscribe(subscribeQueueName, func(m *nats.Msg) {
+	// 	fmt.Printf("Received a message: %s\n", string(m.Data))
+	// })
+
+	var msg = broker.Message{
+		map[string]string{},
+		[]byte("Hello NATS!"),
+	}
+
+	natsBroker.Publish(
+		publishQueueName,
+		&msg,
+	)
 
 	// Register Handlers
-	//hello.RegisterSayHandler(service.Server(), new(Say))
+	// hello.RegisterSayHandler(service.Server(), new(Say))
 
 	// Run server
 	if err := service.Run(); err != nil {
